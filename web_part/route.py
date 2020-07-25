5cb39026e7f9d985c9eed6d218e95f6742669a2d
Safat99 <sarkersafat99@gmail.com>
2020-07-25 06:27:58 +0600

    frontend almost ready

--- web_part/route.py
+++ web_part/route.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
 
+@app.route("/")
 @app.route("/dashboard.html")
 def dashboard():
-	return render_template('dashboard.html')
-	#temperature =30, humidity = 90
+	return render_template('dashboard.html',temperature =40, humidity = 90)
+	
